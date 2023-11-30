b3d54aad7f7d83dc8d87be685f2ed2fb9e2bec49
ren1023 <renyou.1023@gmail.com>
2023-11-30 10:42:33 +0800

    建立作業基礎環境:建資料夾、下載bootstrap、建立導引頁

---
+++ q3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 物件導向練習 </title>
+</head>
+<body>
+    <ul>
+        <li><a href="/q1"> 題組一 </a></li>
+        <li><a href="/q2"> 題組二 </a></li>
+        <li><a href="/q3"> 題組三 </a></li>
+        <li><a href="/q4"> 題組四 </a></li>
+    </ul>
+    
+</body>
+</html>
